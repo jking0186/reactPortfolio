102281d1133f86b43237c5caa6686cdb78be1b6c
Joe King <jking0186@gmail.com>
2020-07-25 13:37:39 -0500

    contact page

--- portfolio/src/components/Portfolio.js
+++ portfolio/src/components/Portfolio.js
@@ -5,6 +5,7 @@ function Portfolio() {
     return (
         <div className="portfolio">
             <div className="container">
+            <h5 className="text-center text-secondary m-5">CHECK OUT SOME OF MY WORK</h5>
                 <div className="row mt-5">
                     <div className="col-sm-4">
                         <div className="card">
